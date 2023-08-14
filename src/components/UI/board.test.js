e647521d2bde06eedc06518251fc3130a1aca268
shania873 <vanaeca@hotmail.com>
2023-08-14 19:32:07 +0200

    add package.json

--- src/components/UI/board.test.js
+++ src/components/UI/board.test.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { render,screen } from '@testing-library/react';
-import Board from './Board';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Board from "./Board";
 
-describe('Board component', () => {
-    test('renders a Board component', () => {
-        render(<Board />);
-        expect(screen.getByTestId('game-board')).toBeInTheDocument();
-    });
+describe("Board component", () => {
+  test("renders a Board component", () => {
+    render(<Board />);
+    expect(screen.getByTestId("game-board")).toBeInTheDocument();
+  });
 });
+
+///jkfldmsjkflds
